98db2870a1ddd7b6e85cbcaa7da1b8323b03ef3d
Sushma Budhathoki <bsushma047@gmail.com>
2025-05-20 18:57:55 +0200

    parameters completed

--- exercises/students/your-name-here/04_parameters.py
+++ exercises/students/your-name-here/04_parameters.py
@@ -1,13 +1,22 @@
-#=======================================
+# =======================================
 # Exercise 04_parameters
-#=======================================
+# =======================================
 # Topic: Parameters
-# Task: Create a parameterized query filtering by any column. 
+# Task: Create a parameterized query filtering by any column.
 # Hint: Create a query in database and copy into the sql_query variable
-#=======================================
+# =======================================
+import pandas as pd
+import pyodbc
 
-sql_query = "SELECT * FROM users WHERE country = ?"
-params = ("Germany",)
+SERVER = 'redi-sandbox-01-sql.database.windows.net'
+DATABASE = 'ReDI-SQL-Fundamentals-01'
+USERNAME = 'Student01'
+PASSWORD = 'StrongPassword!123'
+connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
+
+conn = pyodbc.connect(connectionString)
+sql_query = "SELECT * FROM SalesLT.product WHERE color = ?"
+params = ("Black",)
 df = pd.read_sql(sql_query, conn, params=params)
 
 print(df.head(25))

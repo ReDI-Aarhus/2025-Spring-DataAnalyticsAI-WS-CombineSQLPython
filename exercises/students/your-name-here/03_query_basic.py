d7633e94072bb7149875719626649963bba90025
Sushma Budhathoki <bsushma047@gmail.com>
2025-05-20 18:34:15 +0200

    03 query basic complete

--- exercises/students/your-name-here/03_query_basic.py
+++ exercises/students/your-name-here/03_query_basic.py
@@ -1,12 +1,22 @@
-#=======================================
+# =======================================
 # Exercise 03_query_basic
-#=======================================
-# Topic: Basic queries 
-# Hint: Create a query in database and copy into the dataframe definition. 
-#=======================================
+# =======================================
+# Topic: Basic queries
+# Hint: Create a query in database and copy into the dataframe definition.
+# =======================================
 
 import pandas as pd
+import pyodbc
 
-#df = dataframe
-df = pd.read_sql("", conn)
+SERVER = 'redi-sandbox-01-sql.database.windows.net'
+DATABASE = 'ReDI-SQL-Fundamentals-01'
+USERNAME = 'Student01'
+PASSWORD = 'StrongPassword!123'
+connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
+
+conn = pyodbc.connect(connectionString)
+# df = dataframe
+sql_query = """
+SELECT Name, ProductID FROM SalesLT.Product"""
+df = pd.read_sql(sql_query, conn)
 print(df.head(25))
